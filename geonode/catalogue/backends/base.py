64680d87eca26126abfd8d01db6e26a356ed17a7
Daniel Berry <dberry@boundlessgeo.com>
2016-04-07 09:12:51 -0500

    osgeo-incubation, Added Copyright Headers to all python files

--- geonode/catalogue/backends/base.py
+++ geonode/catalogue/backends/base.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #########################################################################
 #
-# Copyright (C) 2012 Open Source Geospatial Foundation
+# Copyright (C) 2016 OSGeo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,7 +17,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
-
 
 class BaseCatalogueBackend(object):
     """Catalogue abstract base class"""
