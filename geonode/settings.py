925fc3891abec67121820b7980dd8712d9164783
Way Barrios <waybarrios@gmail.com>
2017-06-08 12:30:34 -0500

    Gazetter queue

--- geonode/settings.py
+++ geonode/settings.py
@@ -67,6 +67,8 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['localhost', 'django'])
 # AUTH_IP_WHITELIST = ['192.168.1.158', '192.168.1.159']
 AUTH_IP_WHITELIST = []
 
+QUEUE_INTERVAL = '*/10'
+
 # Make this unique, and don't share it with anybody.
 _DEFAULT_SECRET_KEY = 'myv-y4#7j-d*p-__@j#*3z@!y24fz8%^z2v6atuy4bo9vqr1_a'
 SECRET_KEY = os.getenv('SECRET_KEY', _DEFAULT_SECRET_KEY)
